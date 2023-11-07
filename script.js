3015fc8464a43cffd20e2aa4d3036a1ee26113f3
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-11-07 20:46:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const linkPerfil = document.getElementById("link-perfil");
+const navPerfil = document.getElementById("nav-perfil");
+
+linkPerfil.addEventListener("mouseover", ()=> {
+  navPerfil.style.display = "block"
+})
+
+document.addEventListener('keyup', (e)=> {
+  console.log(e.key);
+  console.log(e.code);
+
+  if (e.code == 'Digit1') {
+    navPerfil.style.display = 'block'
+  }
+
+  else if (e.code == 'Escape') {
+    navPerfil.style.display = 'none'
+  }
+})
