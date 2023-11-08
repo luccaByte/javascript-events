e470c4d461fcbb0237e2ee855662afaab820f185
lucas <lucaston2323@gmail.com>
2023-11-07 21:23:32 -0300

    fix: script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const linkPerfil = document.getElementById("link-perfil");
 const navPerfil = document.getElementById("nav-perfil");
+const linkPerfilDados = document.getElementById('link-perfil-dados');
 
 linkPerfil.addEventListener("mouseover", ()=> {
   navPerfil.style.display = "block"
@@ -9,8 +10,16 @@ document.addEventListener('keyup', (e)=> {
   console.log(e.key);
   console.log(e.code);
 
+  if (navPerfil.style.display == 'block') {
+    if (e.code == 'Digit1') {
+      linkPerfilDados.click();
+    } else if (e.code == 'Digit1') {
+      navPerfil.style.display = 'block'
+    }
+  }
   if (e.code == 'Digit1') {
     navPerfil.style.display = 'block'
+
   } else if (e.code == 'Escape') {
     navPerfil.style.display = 'none'
   }
